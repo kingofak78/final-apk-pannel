12ddee5c0daa1c5ea40df57cb5780982ecdac4dc
kingofak78 <gotakale952@gmail.com>
2025-03-16 07:58:35 -0700

    Update SimInfo.js

--- models/SimInfo.js
+++ models/SimInfo.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const simInfoSchema = new mongoose.Schema({
+const SimInfoSchema = new mongoose.Schema({
     uniqueid: { type: String, required: true, unique: true },
-    sim1Number: { type: String, default: null },
-    sim1Carrier: { type: String, default: null },
-    sim1Slot: { type: Number, default: null },
-    sim2Number: { type: String, default: null },
-    sim2Carrier: { type: String, default: null },
-    sim2Slot: { type: Number, default: null },
-    createdAt: { type: Date, default: Date.now }
-});
+    sim1Number: { type: String, default: '' },
+    sim1Carrier: { type: String, default: '' },
+    sim1Slot: { type: String, default: '' },
+    sim2Number: { type: String, default: '' },
+    sim2Carrier: { type: String, default: '' },
+    sim2Slot: { type: String, default: '' }
+}, { timestamps: true }); // auto-generates createdAt and updatedAt
 
-module.exports = mongoose.model("SimInfo", simInfoSchema);
+module.exports = mongoose.model('SimInfo', SimInfoSchema);
